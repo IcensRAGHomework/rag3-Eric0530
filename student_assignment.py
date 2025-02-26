07a360d2f2bea6acee9b1e3e325e14e50d11f1f8
eric7_chen <eric7_chen@asus.com>
2025-02-26 15:54:42 +0800

    PS/Eric7_Chen 20250226

--- student_assignment.py
+++ student_assignment.py
@@ -17,10 +17,10 @@ def generate_hw01():
     # 讀取 CSV 檔案
     file_path = os.path.join(dbpath, "COA_OpenData.csv")
     df = pd.read_csv(file_path)
-
+     
     # 初始化 ChromaDB 並使用 SQLite 作為存儲
-    db_path = "./"
-    chroma_client = chromadb.PersistentClient(path=db_path)
+    #db_path = "./"
+    chroma_client = chromadb.PersistentClient(path=dbpath)
 
     openai_ef = embedding_functions.OpenAIEmbeddingFunction(
     api_key = gpt_emb_config['api_key'],
@@ -44,15 +44,16 @@ def generate_hw01():
             if not host_words:  # 如果 `HostWords` 為空，則跳過
                 continue
 
+
             metadata = {
             "file_name": "COA_OpenData.csv",
-            "name": row.get("name", ""),
-            "type": row.get("type", ""),
-            "address": row.get("address", ""),
-            "tel": row.get("tel", ""),
-            "city": row.get("city", ""),
-            "town": row.get("town", ""),
-            "date": int(time.mktime(pd.to_datetime(row.get("CreateDate", "1970-01-01")).timetuple()))  # 轉換為時間戳
+            "name": row.get("Name", ""),
+            "type": row.get("Type", ""),
+            "address": row.get("Address", ""),
+            "tel": row.get("Tel", ""),
+            "city": row.get("City", ""),
+            "town": row.get("Town", ""),
+            "date": row.get("CreateDate", ""),
             }
 
             records.append((str(index), host_words, metadata))  # 使用 index 作為 id
@@ -64,13 +65,41 @@ def generate_hw01():
         metadatas=[r[2] for r in records]        # Metadata 附加資訊
         )   
 
-    print(collection.count())
+    print(collection.count)
     return collection
     
 
 def generate_hw02(question, city, store_type, start_date, end_date):
-    pass
-    
+    # 連接到已經建立的 ChromaDB SQLite
+    chroma_client = chromadb.PersistentClient(path=dbpath)
+
+    openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+    api_key = gpt_emb_config['api_key'],
+    api_base = gpt_emb_config['api_base'],
+    api_type = gpt_emb_config['openai_type'],
+    api_version = gpt_emb_config['api_version'],
+    deployment_id = gpt_emb_config['deployment_name']
+    )
+
+    # 取得 Collection
+    collection = chroma_client.get_or_create_collection(
+        name="TRAVEL",
+        metadata={"hnsw:space": "cosine"},
+        embedding_function=openai_ef
+
+        )
+    question_embeding = openai_ef([question])
+
+    result = collection.query(
+        query_embeddings=question_embeding,
+        n_results=10,
+        )
+
+    print(result)
+
+    return result
+
+
 def generate_hw03(question, store_name, new_store_name, city, store_type):
     pass
     
@@ -92,3 +121,6 @@ def demo(question):
     return collection
 
 generate_hw01()
+city=["宜蘭縣", "新北市"]
+type=["美食"]
+#generate_hw02("我想要找有關茶餐點的店家",city,type,"2024-01-01","2024-12-31")
