c84e61ae4db9d7760ada3a63ab0365e069db7e7d
eric7_chen <eric7_chen@asus.com>
2025-03-04 17:12:53 +0800

    PS/Eric7_Chen 202503041513

--- student_assignment.py
+++ student_assignment.py
@@ -188,7 +188,7 @@ def demo(question):
 city=["宜蘭縣", "新北市"]
 city_hw03=["南投縣"]
 store_type=["美食"]
-start_date = datetime.datetime(2024, 1, 1)
-End_date = datetime.datetime(2024, 12, 31)
-#generate_hw02("我想要找有關茶餐點的店家",city,store_type,start_date,End_date)
-generate_hw03("我想要找南投縣的田媽媽餐廳，招牌是蕎麥麵", "耄饕客棧", "田媽媽(耄饕客棧)", city_hw03, store_type)
+start_date = datetime.datetime(2024, 4, 1)
+End_date = datetime.datetime(2024, 5, 1)
+generate_hw02("我想要找有關茶餐點的店家",city,store_type,start_date,End_date)
+#generate_hw03("我想要找南投縣的田媽媽餐廳，招牌是蕎麥麵", "耄饕客棧", "田媽媽(耄饕客棧)", city_hw03, store_type)
